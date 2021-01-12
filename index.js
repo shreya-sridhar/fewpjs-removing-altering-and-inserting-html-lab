0c6a7b3af73551bc9e53bba54bfed069f0a35dcc
shreya-sridhar <shreyasridhar1109@gmail.com>
2021-01-11 17:59:13 -0800

    done

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+let main = document.getElementById('main')
+main.remove();
+
+let newHeader = document.createElement('h1')
+document.body.appendChild(newHeader)
+newHeader.id = 'victory'
+newHeader.innerHTML = "Alexis is the champion";
